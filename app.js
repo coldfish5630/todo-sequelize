1896abae1778c0ab4a46f5bf39633f1959a6de33
coldfish5630 <layan.schicksal@gmail.com>
2022-07-03 17:20:23 +0900

    feat: connect users & todos model

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const port = 3000
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -15,7 +18,11 @@ app.get('/', (req, res) => res.send('Hello World'))
 app.get('/users/login', (req, res) => res.render('login'))
 app.get('/users/register', (req, res) => res.render('register'))
 app.post('/users/login', (req, res) => res.send('login'))
-app.post('/users/register', (req, res) => res.send('register'))
+app.post('/users/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  await User.create({ name, email, password })
+  res.redirect('/')
+})
 app.get('/users/logout', (req, res) => res.render('login'))
 
 app.listen(port, () => console.log(`app is running on port:${port}`))
