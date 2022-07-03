505f822105414a8e1640134c8972e324432318d2
coldfish5630 <layan.schicksal@gmail.com>
2022-07-03 21:43:46 +0900

    feat: add passport & login auth

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const app = express()
 const port = 3000
 const db = require('./models')
@@ -10,18 +13,64 @@ const User = db.User
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: false
+  })
+)
+usePassport(app)
+
+app.get('/', async (req, res) => {
+  try {
+    let todos = await Todo.findAll()
+    todos = todos.map(todo => todo.dataValues)
+    return res.render('index', { todos: todos })
+  } catch (err) {
+    return res.status(422).json(err)
+  }
+})
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const todo = await Todo.findByPk(id)
+    return res.render('detail', { todo: todo.dataValues })
+  } catch (err) {
+    return console.log(err)
+  }
+})
 
 app.get('/users/login', (req, res) => res.render('login'))
 app.get('/users/register', (req, res) => res.render('register'))
-app.post('/users/login', (req, res) => res.send('login'))
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })
+)
 app.post('/users/register', async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  await User.create({ name, email, password })
-  res.redirect('/')
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    const user = await User.findOne({ where: email })
+    if (user) {
+      console.log('user already exists')
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+    })
+    return res.redirect('/')
+  } catch (err) {
+    console.log(err)
+  }
 })
 app.get('/users/logout', (req, res) => res.render('login'))
 
--- seeders/20220703081910-default-data.js
+++ seeders/20220703081910-default-data.js
@@ -1,4 +1,10 @@
-'use strict';
+'use strict'
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -10,7 +16,37 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    const userId = await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: bcrypt.hashSync(
+            SEED_USER.password,
+            bcrypt.genSaltSync(10),
+            null
+          ),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+    await queryInterface.bulkInsert(
+      'Todos',
+      Array.from({ length: 10 }).map(
+        (_, i) => ({
+          name: `name-${i}`,
+          UserId: userId,
+          isDone: false,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }),
+        {}
+      )
+    )
   },
 
   async down (queryInterface, Sequelize) {
@@ -20,5 +56,7 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Todos', null, {})
+    await queryInterface.bulkDelete('Users', null, {})
   }
-};
+}
